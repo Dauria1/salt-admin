b7699cdf20f980216156e69b1ce1b396788e0087
Tyler <tdauria1@gmail.com>
2019-02-01 11:11:13 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Dashboard from './Components/Dashboard/Dashboard';
+import logo from './saltlogo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Route
+              path="/"
+              render={routeProps => (
+                <Dashboard className="dashboard" {...routeProps} />
+              )}
+            />
+
+          </header>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ server/client/build/precache-manifest.15200f08c3132a34f6255121be1d262e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec74e59afc92635130a1f0287f524de0",
+    "url": "/static/media/saltlogo.ec74e59a.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ffbb605ae6352cd62b5c",
+    "url": "/static/js/main.ffbb605a.chunk.js"
+  },
+  {
+    "revision": "4decb4aa3990a2f6b499",
+    "url": "/static/js/1.4decb4aa.chunk.js"
+  },
+  {
+    "revision": "ffbb605ae6352cd62b5c",
+    "url": "/static/css/main.09ae34a4.chunk.css"
+  },
+  {
+    "revision": "f36ea534aa1baf398fe15e6ad174f024",
+    "url": "/index.html"
+  }
+];
